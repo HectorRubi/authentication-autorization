20303e55e84c514178d4b439ec8b98e02afd4a09
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-07-01 18:36:28 -0600

    Add login functionality

--- 02-session-cookies/auth-system/index.js
+++ 02-session-cookies/auth-system/index.js
@@ -5,7 +5,13 @@ const bcrypt = require("bcrypt");
 const app = express();
 const port = 3000;
 const salt = 10;
-const users = [];
+const users = [
+  {
+    id: 1,
+    email: "test@test.com",
+    pass: "$2b$10$KZoGcU.YRpoQk6zhrDxT6uai9nPykaZ58axgg1YSOuEV.5ZocyG8G",
+  },
+];
 
 const getCredentials = (data) => atob(data).split(":");
 
@@ -34,27 +40,40 @@ app.post("/register", (req, res) => {
   // Get credentials
   const [email, pass] = getCredentials(req.body.data);
 
-  // Encrypt password
-  const encrypted = bcrypt.hashSync(pass, salt);
-
   // Check if user is already register
   if (
     !users.find(
-      (user) =>
-        user.email === email && bcrypt.hashSync(user.pass, salt) === encrypted
+      (user) => user.email === email && bcrypt.compareSync(pass, user.pass)
     )
   ) {
     // Save data into database
     const id = users.length + 1;
-    users.push({ id, email, encrypted });
-    req.session.user = { id, email, encrypted };
+    const encrypted = bcrypt.hashSync(pass, salt);
+    users.push({ id, email, pass: encrypted });
+    // req.session.user = { id, email, encrypted };
   }
 
   console.log({ users });
   res.status(201).json({ message: "Registration success" });
 });
 
-app.post("/login", () => {});
+app.post("/login", (req, res) => {
+  // Get credentials
+  const [email, pass] = getCredentials(req.body.data);
+
+  // Check if user exist
+  const user = users.find(
+    (userDB) => userDB.email === email && bcrypt.compareSync(pass, userDB.pass)
+  );
+  if (user) {
+    // Encrypt password
+    const encrypted = bcrypt.hashSync(pass, salt);
+    req.session.user = { id: user.id, email, pass: encrypted };
+    res.status(200).json({ message: "Access granted" });
+  } else {
+    res.status(403).json({ message: "Email or password incorrect" });
+  }
+});
 
 app.post("/logout", () => {});
 
